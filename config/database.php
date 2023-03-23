ee59e4878e3afa693f7c405e4d53f2dbe9ec9bc4
Saiful Islam Meraz <42317343+meraz007@users.noreply.github.com>
2023-03-23 19:47:59 +0600

    try add oop

--- config/database.php
+++ config/database.php
@@ -1,11 +1,30 @@
 <?php 
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'meraz');
-    define('DB_PASS', 'meraz');
-    define('DB_NAME', 'php_dev');
+    // define('DB_HOST', 'localhost');
+    // define('DB_USER', 'meraz');
+    // define('DB_PASS', 'meraz');
+    // define('DB_NAME', 'php_dev');
 
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    // $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-    if($conn->connect_error) {
-        die('Connection Failed' . $conn->connect_error);
+    // if($conn->connect_error) {
+    //     die('Connection Failed' . $conn->connect_error);
+    // }
+
+    class database{
+        private $host;
+        private $dbusername;
+        private $dbpassword;
+        private $dbname;
+        
+        protected function connect(){
+            $this->host='localhost';
+            $this->dbusername='meraz';
+            $this->dbpassword='meraz';
+            $this->dbname='php_dev';
+            $conn=new mysqli($this->host,$this->dbusername,$this->dbpassword,$this->dbname);
+            return $conn;
+
+        }
     }
+
+    
